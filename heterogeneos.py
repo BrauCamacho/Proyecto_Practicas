15dbb5d3181a0ca3b810cc919e3249443ac5e2d3
Braulio <be.camacholoya@ugto.mx>
2021-03-10 17:19:26 -0600

    Diferentes modificaciones, y se adjunta ev 2

--- heterogeneos.py
+++ heterogeneos.py
@@ -3,18 +3,18 @@ from scipy.stats import mode
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
+#se instancia la clase, y se introdicen los estimadores
 class Heterogeneo(BaseEstimator, ClassifierMixin):
     def __init__(self, estimadores):
         self.estimadores = estimadores
-
+#se entrenan los estimadores 
     def fit(self,X,y):
         X, y = check_X_y(X, y)
-        
         self.classes_ = unique_labels(y)
         for modelo, estimador in self.estimadores:
             estimador.fit(X,y)
         return self
-    
+#la siguiente estructura sirve para generar predicciones en base a el modelo gwenerado
     def predict(self,X):
         check_is_fitted(self)
         X = check_array(X)
