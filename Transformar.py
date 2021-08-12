cab2a7a6b4a4ace359b1d3626902b731f4c2cf7d
Braulio <be.camacholoya@ugto.mx>
2021-08-12 13:32:45 -0500

    ultimas modificaciones

--- Transformar.py
+++ Transformar.py
@@ -1,11 +1,13 @@
 import numpy as np
 from scipy.stats import mode
 from sklearn.metrics import accuracy_score, recall_score, f1_score,precision_score, classification_report, confusion_matrix
-from imblearn.metrics import sensitivity_score
+#from imblearn.metrics import sensitivity_score
 from prueba2 import media_desvia
 from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
 #se importan todos los estimadores
 from heterogeneos import Heterogeneo
+from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
@@ -66,7 +68,7 @@ def Datasets():
     datasets.append(datas("Datasets/ThoraricSurgery.txt",16,"TLCS"))
     datasets.append(datas("Datasets/vertebral_column.txt",6,"VC"))
     datasets.append(datas("Datasets/train_data.txt",26,"Parkinson1"))
-    datasets.append(datas("Datasets/wdbc    .txt",0,"BCWD"))
+    datasets.append(datas("Datasets/wdbc.txt",0,"BCWD"))
     datasets.append(datas("Datasets/BCWO.txt",9,"BCWO"))
     datasets.append(datas("Datasets/BCWP.txt",0,"BCWP"))
     datasets.append(datas("Datasets/chronic_kidney_disease.txt",24,"Kidney"))
@@ -87,31 +89,30 @@ metaestimadores = [
     [VotingClassifier(estimadores), "votingClassifier"],
     #[BaggingClassifier(base_estimator=KNeighborsClassifier(n_neighbors=3), n_estimators=100), "Bagging"],
     [AdaBoostClassifier(n_estimators=100, random_state=True), "AdaBoost"],
-    [RandomForestClassifier(n_estimators=100 , max_depth=5, bootstrap=True), "RandomForest"]
+    [RandomForestClassifier(n_estimators=100 , max_depth=5, bootstrap=True), "RandomForest"],
+    [DecisionTreeClassifier(max_depth=5), "DecisionThree"],
+    [SVC(gamma=1.0, C=1.0, probability=True), "SupportVector"],
+    [KNeighborsClassifier(n_neighbors=3), "KNeighbors"],
+    [GaussianNB(), "GaussianNB"]
 ]
     
 #kf = KFold(n_splits=10)
-kf = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=1)
+kf = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=0)
+scaler = StandardScaler()
 for Dataset in Datasets():
     #print(Dataset.name)
     X,y = load_creado(Dataset)
     res = open(f"Resultados/{Dataset.name}.csv", "w")
     res.write(f"Estimador,Datos,Accuracy_score,recall_score_1,recall_score_2,f1_score_1,f1_score_2,precision_score_1,precision_score_2\n")
-    for meta in metaestimadores:
-        #print(meta[1])
-        for train_index, test_index in kf.split(X,y):
+    for train_index, test_index in kf.split(X,y):
+        scaler.fit(X[train_index])
+        X[train_index] = scaler.fit_transform(X[train_index])
+        X[test_index] = scaler.fit_transform(X[test_index])
+        for meta in metaestimadores:    
             meta[0].fit(X[train_index],y[train_index])
             train = meta[0].predict(X[train_index])
             res.write(metrics(y[train_index], train, meta[1], "Entrenamiento"))
-            #print("entrenamiento")
-            #print("Accuracy ",accuracy_score(y_true=y[train_index], y_pred=train))
-            #print("Recall ", recall_score(y_true=y[train_index], y_pred=train, average=None))
-            #print(confusion_matrix(y_true=y[train_index], y_pred=train, ))
             test = meta[0].predict(X[test_index])
-            #print("prueba")
-            #print("Accuracy ",accuracy_score(y_true=y[test_index], y_pred=test))
-            #print("Recall ",recall_score(y_true=y[test_index], y_pred=test, average=None))
-            #print(confusion_matrix(y_true=y[test_index], y_pred=test))
             res.write(metrics(y[test_index], test, meta[1], "Prueba"))
     res.close()
     
